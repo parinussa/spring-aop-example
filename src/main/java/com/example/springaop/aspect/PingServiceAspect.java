75a207ac63335104f6c2aa17a03d55a903fc0734
Noval Parinussa <novalparinussa@gmail.com>
2020-03-12 17:13:26 +0700

    Add after advice to Ping service

--- src/main/java/com/example/springaop/aspect/PingServiceAspect.java
+++ src/main/java/com/example/springaop/aspect/PingServiceAspect.java
@@ -1,5 +1,7 @@
 package com.example.springaop.aspect;
 
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
@@ -11,6 +13,11 @@ public class PingServiceAspect {
     @Before("execution(* getPing())")
     public void checkSomethingAdvice() {
         System.out.println("(before advice) Doing some checking here...");
+    }
+
+    @After("execution(* getPing())")
+    public void logDataAdvice() {
+        System.out.println("(after advice) Ping service called");
     }
 
 }
